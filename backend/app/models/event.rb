b66690ea0c0d97c7d7ba626c3537a3f9a26a6e5d
VicenteGiaconi <vgiaconi@miuandes.cl>
2024-08-21 21:26:04 -0400

    feat: solving problems (for real)

--- backend/app/models/event.rb
+++ backend/app/models/event.rb
@@ -5,13 +5,6 @@ class Event < ApplicationRecord
 
   has_one_attached :flyer
 
-<<<<<<< HEAD
-  validates :flyer, content_type: { in: ['image/png', 'image/jpg', 'image/jpeg'],
-                                    message: 'must be a valid flyer format' },
-                    size: { less_than: 5.megabytes }
-
-=======
->>>>>>> upstream/main
   def thumbnail
     flyer.variant(resize_to_limit: [200, nil]).processed
   end  
